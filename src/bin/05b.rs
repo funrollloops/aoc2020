43fef775e6e52ea8d864c5d5ec357daf877eee17
Sagar Mittal <sagar@sagarmittal.com>
2022-07-30 23:46:07 -0700

    Simplify 5b

--- src/bin/05b.rs
+++ src/bin/05b.rs
@@ -3,10 +3,10 @@ fn main() {
 }
 
 fn try_main() -> anyhow::Result<()> {
-    let mut ids: Vec<i16> = include_str!("../../inputs/05.txt").lines().map(|l| {
-        let d = decode(l).unwrap();
-        d.0 * 8 + d.1
-    }).collect();
+    let mut ids = include_str!("../../inputs/05.txt")
+        .lines()
+        .map(decode)
+        .collect::<anyhow::Result<Vec<_>>>()?;
     ids.sort();
     for i in 0..(ids.len() - 1) {
         if ids[i] + 2 == ids[i + 1] {
@@ -16,22 +16,21 @@ fn try_main() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn decode(s: &str) -> anyhow::Result<(i16, i16)> {
+fn decode(s: &str) -> anyhow::Result<i16> {
     assert!(s.len() == 10);
     let s = s.as_bytes();
-    let mut row: i16 = 0;
+    let mut id: i16 = 0;
     for i in 0..7 {
-        row <<= 1;
+        id <<= 1;
         if s[i] == b'B' {
-            row |= 1;
+            id |= 1;
         }
     }
-    let mut col: i16 = 0;
     for i in 0..3 {
-        col <<= 1;
+        id <<= 1;
         if s[7 + i] == b'R' {
-            col |= 1;
+            id |= 1;
         }
     }
-    Ok((row, col))
+    Ok(id)
 }
